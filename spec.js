35b51196ab9dcc9b8181875146f9f5c7652522a5
“maria.lutskevich” <maria_lutskevich@epam.com>
2021-12-01 21:40:24 +0300

    Updates

--- spec.js
+++ spec.js
@@ -10,49 +10,42 @@ describe('Protractor Demo', function () {
     const username = element(by.xpath(`//*[@id="loginForm:j_username"]`));
     const password = element(by.xpath(`//*[@id="loginForm:j_password"]`));
 
-    beforeEach(function () {
-        browser.get('http://localhost:8080/ipb-app');
+    beforeEach(async function () {
+        await browser.get('http://localhost:8080/ipb-app');
     });
 
-    it('should Login', function () {
-        username.sendKeys('qa');
-        password.sendKeys('qa');
-        element(by.name('loginForm:submitForm')).click();
-
-        expect(browser.getTitle()).toEqual('MY WORK (Build: 12.17-SNAPSHOT)');
+    it('should Login', async function () {
+        await username.sendKeys('qa');
+        await password.sendKeys('qa');
+        await element(by.name('loginForm:submitForm')).click();
+        expect(await browser.getTitle()).toEqual('MY WORK (Build: 12.17-SNAPSHOT)');
     });
 
-    it('should find Customer', function () {
-        element(by.name('topQuickSearchForm:request')).sendKeys('510022');
-        element(by.name('topQuickSearchForm:quickSearchBtn')).click();
-        return new Promise(resolve => {
-            setTimeout(resolve, 3000);
-        });
-
-        expect(browser.getTitle()).toEqual('Customer View (Build: 12.17-SNAPSHOT)');
+    it('should find Customer', async function () {
+        await element(by.name('topQuickSearchForm:request')).sendKeys('510022');
+        await element(by.name('topQuickSearchForm:quickSearchBtn')).click();
+        await browser.sleep(3000);
+        expect(await browser.getTitle()).toEqual('Customer View (Build: 12.17-SNAPSHOT)');
     });
 
-    it('should navigate to My Work', function () {
-        return new Promise(resolve => {
-            setTimeout(resolve, 1000);
-        });
-        element(by.xpath(`//*[@text="My Work"]`)).click();
-
+    it('should navigate to My Work', async function () {
+        await browser.sleep(6000);
+        await element(by.xpath(`//*[@id="tabForm:topTabsBarList:0:linkLabel"]`)).click();
         expect(element(by.xpath('//form[@id="taskManagingForm"]')).isPresent()).toBeTruthy();
     });
 
-    it('should check new tasks', function () {
-        element(by.name('taskControlsForm:getNextTask')).click();
-        return new Promise(resolve => {
-            setTimeout(resolve, 1000);
-        });
-
-        expect(element(by.xpath('//[@id="noNextTaskPopup"]')).isPresent()).toBeTruthy();
+    it('should check new tasks', async function () {
+        await browser.sleep(2000);
+        await element(by.xpath(`//*[@id="taskControlsForm:getNextTask"]`)).click();
+        await browser.sleep(3000);
+        expect(element(by.xpath('//*[@id="noNextTaskPopup"]')).isPresent()).toBeTruthy();
     });
 
-    it('should close pop-up', function () {
-        element(by.name('noNextTaskPopupButtonOk')).click();
-        browser.sleep(3000);
-        expect(browser.getTitle()).toEqual('MY WORK (Build: 12.17-SNAPSHOT)');
+    it('should close pop-up', async function () {
+        await browser.sleep(3000);
+        const elm = await element(by.xpath(`//*[@id="noNextTaskPopup"]//table//*[@id="noNextTaskPopupButtonOk"]`));
+        await browser.executeScript("arguments[0].click();", elm.getWebElement());
+        await browser.sleep(3000);
+        await expect(await browser.getTitle()).toEqual('MY WORK (Build: 12.17-SNAPSHOT)');
     });
 });
